011537b0dfa3c501772ad3c1b25e01055fe08734
d2933 <85583554+d2933@users.noreply.github.com>
2024-06-27 14:57:06 +0530

    Delete connection.js

--- connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery",true);
-async function connectwithmongodb(url){
-        mongoose.connect(url)  //It returns a Promise 
-        .then(()=>(console.log("Mongo Connected")))
-        .catch((err)=>console.log("Mongo error",err));
-}
-
-module.exports={connectwithmongodb};
